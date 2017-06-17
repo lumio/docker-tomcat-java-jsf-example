7448a84548ff6ba213cbbd94b3ac75ed33fe21f1
Maik Hilker <maikhilker89@gmail.com>
2017-06-17 16:11:08 +0200

    added docblocks

--- src/main/java/com/github/mhilker/example/HelloWorldServlet.java
+++ src/main/java/com/github/mhilker/example/HelloWorldServlet.java
@@ -7,8 +7,19 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/**
+ * This servlet responses with "Hello World!" to GET requests.
+ */
 public class HelloWorldServlet extends HttpServlet {
 	
+	/**
+	 * Write "Hello World!" to the response.
+	 * 
+	 * @param request
+	 * @param response
+	 * @throws ServletException
+	 * @throws IOException
+	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter writer = response.getWriter();
 		
