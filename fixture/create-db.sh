eec9cc4f057fe896498bf44b566160211f979c3f
Benjamin Falk <lumio@lumio.at>
2020-10-04 22:13:25 +0200

    feat: #1 remove fixtures

--- fixture/create-db.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-selfdir=$(dirname "$0")
-sql_file="$selfdir/dump.sql"
-current_dir=$(pwd)
-cd "$selfdir/../data"
-data_path=$(pwd)
-cd "$current_dir"
-docker run -i --rm -v "$data_path:/data" nouchka/sqlite3 /data/shop.db < "$sql_file"
